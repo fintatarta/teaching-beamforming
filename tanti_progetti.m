4604498abd8fdf567da56b3fe51f1e8c978b3350
Riccardo Bernardini <riccardo.bernardini@uniud.it>
2019-04-30 16:33:53 +0200

    Updated tanti_progetti to save data in file

--- tanti_progetti.m
+++ tanti_progetti.m
@@ -3,15 +3,16 @@ distanza_m = 5.0e-2;
 frequenza_Hz = 3000;
 angoli = -45:5:45;
 margine = 15;
-
+N = 8;
 filtri = [];
 
 for alfa = angoli
-  [h, A, theta] = progetta(alfa + margine*[-1,1], 8, distanza_m, frequenza_Hz);
+  [h, A, theta] = progetta(alfa + margine*[-1,1], N, distanza_m, frequenza_Hz);
   filtri = [filtri h];
   plot(theta, abs(A));
   hold on;
 end
 
 tmp = [angoli ; filtri];
+tmp = [N; tmp(:)];
 save filtri.txt tmp
